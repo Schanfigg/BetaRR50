dd6bb9ef15ac21fcd28e1a5718e363035494a52b
Schanfigg <154096842+Schanfigg@users.noreply.github.com>
2024-08-02 22:38:52 +0200

    Update BetaRR50.ino

--- BetaRR50.ino
+++ BetaRR50.ino
@@ -1,8 +1,7 @@
 #include <ArduinoBLE.h>
 #include "ArduinoGraphics.h"
 #include "Arduino_LED_Matrix.h"
-#include "Frames.h"
-#include "version_info.ino"  // Einbinden der Versionsdatei
+#include "frames.h"
 
 ArduinoLEDMatrix matrix;
 
@@ -12,10 +11,6 @@ BLEService arrowService("19B10010-E8F2-537E-4F6C-D104768A1214"); // BLE Service
 
 BLEByteCharacteristic leftArrowCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLEWrite); // Characteristic for left arrow
 BLEByteCharacteristic rightArrowCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLEWrite); // Characteristic for right arrow
-BLEStringCharacteristic firmwareVersionCharacteristic("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite, 20); // Characteristic for firmware version
-BLEStringCharacteristic firmwareDataCharacteristic("19B10014-E8F2-537E-4F6C-D104768A1214", BLEWrite, 512); // Characteristic for firmware data
-
-const char* currentFirmwareVersion = "V1.0"; // Current firmware version
 
 unsigned long arrowDisplayStartTime = 0;
 bool displayingArrow = false;
@@ -49,14 +44,11 @@ void setup() {
 
   arrowService.addCharacteristic(leftArrowCharacteristic);
   arrowService.addCharacteristic(rightArrowCharacteristic);
-  arrowService.addCharacteristic(firmwareVersionCharacteristic);
-  arrowService.addCharacteristic(firmwareDataCharacteristic);
 
   BLE.addService(arrowService);
 
   leftArrowCharacteristic.writeValue(0);
   rightArrowCharacteristic.writeValue(0);
-  firmwareVersionCharacteristic.writeValue(currentFirmwareVersion);
 
   BLE.advertise();
 }
@@ -77,11 +69,7 @@ void loop() {
         // Turn off the display
         delay(5000);
         matrix.clear();
-      }
-      if (firmwareDataCharacteristic.written()) {
-        String firmwareData = firmwareDataCharacteristic.value();
-        // Handle received firmware data
-        processFirmwareData(firmwareData);
+        
       }
     }
   }
@@ -95,11 +83,4 @@ void loop() {
     matrix.clear();
     delay(1000);
   }
-}
-
-void processFirmwareData(const String &data) {
-  // This is where you'd handle the received firmware data and write it to the appropriate storage
-  // For now, we'll just print it to the serial console
-  Serial.println("Received firmware data:");
-  Serial.println(data);
 }
