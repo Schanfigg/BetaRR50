98aa4cae11bf82a330f3b67cc7e0c3bcfb71b2ba
Schanfigg <154096842+Schanfigg@users.noreply.github.com>
2024-08-02 22:39:08 +0200

    Update Frames.h

--- Frames.h
+++ Frames.h
@@ -1,11 +1,17 @@
-const uint32_t Right[] = {
+const uint32_t left_frame[] = {
   0x400c01c,
 	0x3fc3fc1,
 	0xc00c0040
 };
 
-const uint32_t Left[] = {
+const uint32_t right_frame[] = {
   0x2003003,
 	0x83fc3fc0,
 	0x38030020
+};
+
+const uint32_t bluetooth_logo[] = {
+  0x10829,
+	0x44627fe0,
+	0x60090108
 };
